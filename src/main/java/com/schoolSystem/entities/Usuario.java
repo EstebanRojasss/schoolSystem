5cfeef576e8c348aa9c3257925770671e23b3d1e
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-09 14:29:47 -0300

    fix: Agregar final en atributo Id

---
+++ src/main/java/com/schoolSystem/entities/Usuario.java
@@ -0,0 +1,68 @@
+package com.schoolSystem.entities;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private final Long id;
+    private String nombreUsuario;
+    private String contrasenha;
+    private String email;
+    private LocalDate fechaRegistro;
+    private Estado estado;
+
+    public Usuario(Long id, String nombreUsuario, String contrasenha, String email, LocalDate fechaRegistro, Estado estado) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenha = contrasenha;
+        this.email = email;
+        this.fechaRegistro = fechaRegistro;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+
+
+}
