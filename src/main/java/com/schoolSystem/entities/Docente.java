b77a4b09b86542a61057eb45a789c0979aa584f8
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-10 00:05:24 -0300

    feat: Agregar anotaciones para el mapeo y persistencia en la clase Docente

--- src/main/java/com/schoolSystem/entities/Docente.java
+++ src/main/java/com/schoolSystem/entities/Docente.java
@@ -1,17 +1,38 @@
 package com.schoolSystem.entities;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
 import java.time.LocalDate;
 
-public class Docente extends Persona{
+public class Docente extends Persona {
+
+    @Enumerated(EnumType.STRING)
     private Estado estado;
+
+    @OneToOne
+    @JoinColumn(name = "usuario", unique = true, nullable = false)
     private Usuario usuario;
 
-    public Docente(Long id, String nombre, String apellido, String numeroDocumento, LocalDate fechaNacimiento, String direccion, String telefono, String email, Estado estado) {
+
+    public Docente(Long id, String nombre, String apellido, String numeroDocumento, LocalDate fechaNacimiento, String direccion, String telefono, String email, Estado estado, Usuario usuario) {
         super(id, nombre, apellido, numeroDocumento, fechaNacimiento, direccion, telefono, email);
         this.estado = estado;
+        this.usuario = usuario;
     }
+
     public Docente() {
 
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     public Estado getEstado() {
