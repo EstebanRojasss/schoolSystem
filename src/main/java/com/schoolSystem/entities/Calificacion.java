3f6309afd4f8e15b030e0dceeb660ec64905e5a2
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-10 19:53:26 -0300

    chore: Agregar anotaciones JPA en la clase Calificacion

--- src/main/java/com/schoolSystem/entities/Calificacion.java
+++ src/main/java/com/schoolSystem/entities/Calificacion.java
@@ -1,9 +1,20 @@
 package com.schoolSystem.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Calificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "nota")
     private Short nota;
+
+    @ManyToOne
+    @JoinColumn(name = "idEstudiante", nullable = false)
     private Estudiante estudiante;
+    @ManyToOne
+    @JoinColumn(name = "idCurso", nullable = false)
     private Curso curso;
 
 }
