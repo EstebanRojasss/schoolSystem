982ccb3067ecb7c0a25fe634a2cb47908abb3037
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-11 01:02:13 -0300

    chore: Agregar anotaciones JPA a la clase Docente

--- src/main/java/com/schoolSystem/entities/Docente.java
+++ src/main/java/com/schoolSystem/entities/Docente.java
@@ -3,6 +3,8 @@ package com.schoolSystem.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Set;
+
 @Entity
 @Table(name = "docente")
 public class Docente extends Persona {
@@ -14,11 +16,27 @@ public class Docente extends Persona {
     @JoinColumn(name = "usuario", unique = true, nullable = false)
     private Usuario idUsuario;
 
+    @ManyToMany
+    @JoinTable(
+            name = "docente_curso",
+            joinColumns = @JoinColumn(name = "docente_id"),
+            inverseJoinColumns = @JoinColumn(name = "curso_id")
+    )
+    private Set<Curso> cursos;
 
-    public Docente(Long id, String nombre, String apellido, String numeroDocumento, LocalDate fechaNacimiento, String direccion, String telefono, String email, Estado estado, Usuario idUsuario) {
+    public Docente(Long id, String nombre, String apellido, String numeroDocumento, LocalDate fechaNacimiento, String direccion, String telefono, String email, Estado estado, Usuario idUsuario, Set<Curso> cursos) {
         super(id, nombre, apellido, numeroDocumento, fechaNacimiento, direccion, telefono, email);
         this.estado = estado;
         this.idUsuario = idUsuario;
+        this.cursos = cursos;
+    }
+
+    public Set<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(Set<Curso> cursos) {
+        this.cursos = cursos;
     }
 
     public Docente() {
