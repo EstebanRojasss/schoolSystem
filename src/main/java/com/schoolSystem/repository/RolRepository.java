2359b2426e51dfd1cc43d1208ab51467b73fb1e9
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-12 00:14:26 -0300

    feat: Agregar repositorio para la clase Rol

---
+++ src/main/java/com/schoolSystem/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.schoolSystem.repository;
+
+import com.schoolSystem.entities.rol.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+}
