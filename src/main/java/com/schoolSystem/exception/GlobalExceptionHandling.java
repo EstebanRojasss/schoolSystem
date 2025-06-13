b2a99f5d82737be20aabd0d0c21bccd403d7ecd2
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-13 11:05:30 -0300

    feat: Crear nueva clase para el manejo global de exepciones

---
+++ src/main/java/com/schoolSystem/exception/GlobalExceptionHandling.java
@@ -0,0 +1,22 @@
+package com.schoolSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String>notFoundHandle(UserNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RoleNotFound.class)
+    public ResponseEntity<String>notFoundHandle(RoleNotFound roleNotFound){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(roleNotFound.getMessage());
+    }
+
+    
+}
