96a035947418e14f57f6f8d1aafa0d1075544839
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-13 11:13:25 -0300

    feat: Crear clase controlador y endpoint para la creacion de nuevo usuario

--- src/main/java/com/schoolSystem/controllers/UsuarioController.java
+++ src/main/java/com/schoolSystem/controllers/UsuarioController.java
@@ -1,5 +1,26 @@
 package com.schoolSystem.controllers;
 
+import com.schoolSystem.dto.UsuarioDto;
+import com.schoolSystem.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/admin")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UsuarioDto usuarioDto){
+        usuarioService.createUser(usuarioDto);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
