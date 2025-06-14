46d2282fff14cc9aba7d796113ec1e6e08430d3b
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-14 01:11:27 -0300

    feat: Crear clase para el manejo de excepcion en caso de no encontrar el email en la base

---
+++ src/main/java/com/schoolSystem/exception/EmailNotFoundException.java
@@ -0,0 +1,8 @@
+package com.schoolSystem.exception;
+
+public class EmailNotFoundException extends BaseException{
+
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
