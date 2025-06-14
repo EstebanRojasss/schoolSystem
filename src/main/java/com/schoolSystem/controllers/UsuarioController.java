3fab47a17936e7b4eb20785021300bbfb91fff6d
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-13 21:54:47 -0300

    chore: Agregar nuevo atributo mapper en la clase UsuarioController mediante inyeccion de depencias con el constructor

--- src/main/java/com/schoolSystem/controllers/UsuarioController.java
+++ src/main/java/com/schoolSystem/controllers/UsuarioController.java
@@ -1,26 +1,37 @@
 package com.schoolSystem.controllers;
 
-import com.schoolSystem.dto.UsuarioDto;
+import com.schoolSystem.dto.UsuarioCreateDto;
+import com.schoolSystem.dto.UsuarioGetDto;
+import com.schoolSystem.mapper.UsuarioToUsuarioDTO;
 import com.schoolSystem.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestController("/admin")
+import java.util.List;
+
+@RestController("/users")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService){
+    private final UsuarioToUsuarioDTO mapper;
+
+    public UsuarioController(UsuarioService usuarioService, UsuarioToUsuarioDTO mapper) {
         this.usuarioService = usuarioService;
+        this.mapper = mapper;
     }
 
-    @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody UsuarioDto usuarioDto){
-        usuarioService.createUser(usuarioDto);
+    @PostMapping("/admin")
+    public ResponseEntity<Void> createUser(@RequestBody UsuarioCreateDto usuarioCreateDto){
+        usuarioService.createUser(usuarioCreateDto);
         return ResponseEntity.ok().build();
     }
+
+    
 
 
 }
