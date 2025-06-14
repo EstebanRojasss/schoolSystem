931e050d3b9b06af47e229bd164380e3ea3f3a0f
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-13 23:43:18 -0300

    feat: Agregar nuevo metodo para encontrar usuario por email en UsuarioRepository

--- src/main/java/com/schoolSystem/repository/UsuarioRepository.java
+++ src/main/java/com/schoolSystem/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package com.schoolSystem.repository;
 import com.schoolSystem.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario>findByEmail(String email);
 }
