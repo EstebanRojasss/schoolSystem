fb59d3935cd71865cc2635ab87d7a759e34ef348
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-16 03:43:13 -0300

    refactor: Refactorizar el nombre del controlador UsuarioController a AdminController

--- src/main/java/com/schoolSystem/controllers/AdminController.java
+++ src/main/java/com/schoolSystem/controllers/AdminController.java
@@ -3,7 +3,6 @@ package com.schoolSystem.controllers;
 import com.schoolSystem.dto.UsuarioCreateDto;
 import com.schoolSystem.dto.UsuarioGetDto;
 import com.schoolSystem.dto.UsuarioUpdateDto;
-import com.schoolSystem.entities.Usuario;
 import com.schoolSystem.mapper.UsuarioToUsuarioDTO;
 import com.schoolSystem.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@RestController("/users")
-public class UsuarioController {
+@RestController("/admin")
+public class AdminController {
 
     private final UsuarioService usuarioService;
 
     private final UsuarioToUsuarioDTO mapUsuarioDto;
 
-    public UsuarioController(UsuarioService usuarioService, UsuarioToUsuarioDTO mapUsuarioDto) {
+    public AdminController(UsuarioService usuarioService, UsuarioToUsuarioDTO mapUsuarioDto) {
         this.usuarioService = usuarioService;
         this.mapUsuarioDto = mapUsuarioDto;
     }
