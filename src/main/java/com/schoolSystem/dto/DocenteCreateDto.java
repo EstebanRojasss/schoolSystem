e69d31f601582d6b8c499b42917b1cb9f2e334ea
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-17 21:32:21 -0300

    feat: Agreagar nuevo dto para creacion de docente

---
+++ src/main/java/com/schoolSystem/dto/DocenteCreateDto.java
@@ -0,0 +1,18 @@
+package com.schoolSystem.dto;
+
+import com.schoolSystem.entities.Curso;
+import com.schoolSystem.entities.Usuario;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public record DocenteCreateDto (String nombre,
+                                String apellido,
+                                String numeroDocumento,
+                                LocalDate fecha_nacimiento,
+                                String direccion,
+                                String telefono,
+                                String email,
+                                Set<Curso>cursos,
+                                Usuario usuario){
+}
