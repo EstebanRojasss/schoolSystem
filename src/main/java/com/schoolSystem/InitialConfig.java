e952365e60f26c605b8e8680afdf0c7b53285241
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-19 04:13:57 -0300

    Clase solo de prueba para la persistencia de datos que va cambiando constantemente

--- src/main/java/com/schoolSystem/InitialConfig.java
+++ src/main/java/com/schoolSystem/InitialConfig.java
@@ -1,61 +1,61 @@
-package com.schoolSystem;
-
-import com.schoolSystem.entities.Curso;
-import com.schoolSystem.entities.rol.Rol;
-import com.schoolSystem.entities.rol.TipoRol;
-import com.schoolSystem.repository.CursoRepository;
-import com.schoolSystem.repository.RolRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Service;
-
-import java.util.Random;
-
-@Service
-public class InitialConfig {
-
-
-    private final RolRepository rolRepository;
-
-    private final CursoRepository cursoRepository;
-
-    public InitialConfig(RolRepository rolRepository, CursoRepository cursoRepository) {
-        this.rolRepository = rolRepository;
-        this.cursoRepository = cursoRepository;
-    }
-
-
-    @PostConstruct
-    public void inicializarDatos() {
-        cargarRolesBD();
-        cargarCursosBD();
-    }
-
-    public void cargarRolesBD() {
-        for (TipoRol tipoRol : TipoRol.values()) {
-            Rol rol = new Rol();
-            rol.setTipoRol(tipoRol);
-            rolRepository.save(rol);
-        }
-    }
-
-    public void cargarCursosBD() {
-        for (String nombre : nombresCurso()) {
-            Curso curso = new Curso();
-            curso.setNombreCurso(nombre);
-            curso.setCreditos(cantidadCreditosCurso());
-            cursoRepository.save(curso);
-        }
-    }
-
-
-    private String[] nombresCurso() {
-        return new String[]{"Primero", "Segundo", "Tercero", "Cuarto", "Quinto"};
-    }
-
-    private short cantidadCreditosCurso() {
-        Random creditosRandom = new Random();
-        return (short) creditosRandom.nextInt(50, 60);
-    }
-
-
-}
+//package com.schoolSystem;
+//
+//import com.schoolSystem.entities.Curso;
+//import com.schoolSystem.entities.rol.Rol;
+//import com.schoolSystem.entities.rol.TipoRol;
+//import com.schoolSystem.repository.CursoRepository;
+//import com.schoolSystem.repository.RolRepository;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Random;
+//
+//@Service
+//public class InitialConfig {
+//
+//
+//    private final RolRepository rolRepository;
+//
+//    private final CursoRepository cursoRepository;
+//
+//    public InitialConfig(RolRepository rolRepository, CursoRepository cursoRepository) {
+//        this.rolRepository = rolRepository;
+//        this.cursoRepository = cursoRepository;
+//    }
+//
+//
+//    @PostConstruct
+//    public void inicializarDatos() {
+//        cargarRolesBD();
+//        cargarCursosBD();
+//    }
+//
+//    public void cargarRolesBD() {
+//        for (TipoRol tipoRol : TipoRol.values()) {
+//            Rol rol = new Rol();
+//            rol.setTipoRol(tipoRol);
+//            rolRepository.save(rol);
+//        }
+//    }
+//
+//    public void cargarCursosBD() {
+//        for (String nombre : nombresCurso()) {
+//            Curso curso = new Curso();
+//            curso.setNombreCurso(nombre);
+//            curso.setCreditos(cantidadCreditosCurso());
+//            cursoRepository.save(curso);
+//        }
+//    }
+//
+//
+//    private String[] nombresCurso() {
+//        return new String[]{"Primero", "Segundo", "Tercero", "Cuarto", "Quinto"};
+//    }
+//
+//    private short cantidadCreditosCurso() {
+//        Random creditosRandom = new Random();
+//        return (short) creditosRandom.nextInt(50, 60);
+//    }
+//
+//
+//}
