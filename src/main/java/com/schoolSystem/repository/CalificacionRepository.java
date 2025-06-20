a6a4b538153c81b45414351118da7adbb22c666c
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-20 17:28:13 -0300

    feat: Agregar nuevo repositorio para clase calificacion

---
+++ src/main/java/com/schoolSystem/repository/CalificacionRepository.java
@@ -0,0 +1,9 @@
+package com.schoolSystem.repository;
+
+import com.schoolSystem.entities.Calificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalificacionRepository extends JpaRepository<Calificacion, Long> {
+
+    
+}
