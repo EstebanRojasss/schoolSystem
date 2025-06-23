4ecf2779621b1be930047f798b40e99f09afa2fc
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-22 23:13:00 -0300

    feat: Agregar endpoint para listar todos los cursos de un docente

--- src/main/java/com/schoolSystem/controllers/UsuarioController.java
+++ src/main/java/com/schoolSystem/controllers/UsuarioController.java
@@ -1,10 +1,28 @@
-//package com.schoolSystem.controllers;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/users")
-//public class UsuarioController {
-//
-//}
+package com.schoolSystem.controllers;
+
+import com.schoolSystem.dto.CursoGetDto;
+import com.schoolSystem.service.DocenteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+    private final DocenteService docenteService;
+
+    public UsuarioController(DocenteService docenteService){
+        this.docenteService = docenteService;
+    }
+
+    @GetMapping("/teacher")
+    public ResponseEntity<Set<CursoGetDto>> getTeacherCourses(@RequestParam Long id){
+        return ResponseEntity.ok( docenteService.getDocenteCursos(id) );
+    }
+
+}
