569d1e57c7e9a2d1b471e047f95841d0d5bfcb9e
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-22 23:41:03 -0300

    feat: Agregar nuevo endpoint para asignar calificacion a un estudiante

--- src/main/java/com/schoolSystem/controllers/UsuarioController.java
+++ src/main/java/com/schoolSystem/controllers/UsuarioController.java
@@ -3,10 +3,7 @@ package com.schoolSystem.controllers;
 import com.schoolSystem.dto.CursoGetDto;
 import com.schoolSystem.service.DocenteService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -16,13 +13,19 @@ public class UsuarioController {
 
     private final DocenteService docenteService;
 
-    public UsuarioController(DocenteService docenteService){
+    public UsuarioController(DocenteService docenteService) {
         this.docenteService = docenteService;
     }
 
     @GetMapping("/teacher")
-    public ResponseEntity<Set<CursoGetDto>> getTeacherCourses(@RequestParam Long id){
-        return ResponseEntity.ok( docenteService.getDocenteCursos(id) );
+    public ResponseEntity<Set<CursoGetDto>> getTeacherCourses(@RequestParam Long id) {
+        return ResponseEntity.ok(docenteService.getDocenteCursos(id));
+    }
+
+    @PostMapping("/teacher/calificacion")
+    public ResponseEntity<String>setGrade(@RequestParam Long studentId, Short nota, String nombreCurso){
+        docenteService.setCalificacionByStudentId(studentId, nota, nombreCurso);
+        return ResponseEntity.ok("La calificación se asignó correctamente");
     }
 
 }
