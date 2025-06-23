fc603ffbccc9d0e43f395874efe1a8c94194ed00
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-06-22 21:00:04 -0300

    chore: Agregar metodos getter y setter los atributos de la clase Calificacion

--- src/main/java/com/schoolSystem/entities/Calificacion.java
+++ src/main/java/com/schoolSystem/entities/Calificacion.java
@@ -18,4 +18,38 @@ public class Calificacion {
     @JoinColumn(name = "idCurso", nullable = false)
     private Curso curso;
 
+
+    public Calificacion(Short nota, Estudiante estudiante, Curso curso) {
+        this.nota = nota;
+        this.estudiante = estudiante;
+        this.curso = curso;
+    }
+
+    public Short getNota() {
+        return nota;
+    }
+
+    public void setNota(Short nota) {
+        this.nota = nota;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
