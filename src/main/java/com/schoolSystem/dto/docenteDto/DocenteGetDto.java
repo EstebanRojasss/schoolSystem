b437840cb40addae5988b561cd5cfb32e5024513
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-14 19:59:30 -0300

    feat: Agregar nuevo DocenteDto para obtener datos de entidad Docente

---
+++ src/main/java/com/schoolSystem/dto/docenteDto/DocenteGetDto.java
@@ -0,0 +1,9 @@
+package com.schoolSystem.dto.docenteDto;
+
+import java.util.Set;
+
+public record DocenteGetDto(String nombre,
+                            String apellido,
+                            String telefono,
+                            String direccion) {
+}
---
+++ src/main/java/com/schoolSystem/mapper/DocenteToDocenteGetDto.java
@@ -0,0 +1,4 @@
+package com.schoolSystem.mapper;
+
+public class DocenteToDocenteGetDto {
+}
