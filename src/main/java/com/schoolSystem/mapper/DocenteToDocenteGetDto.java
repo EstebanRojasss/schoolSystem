f041183f233654fa67b26e1b24dc87a1907c7a17
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-14 20:02:09 -0300

    feat: Agregar nueva clase para el mapeo de entidad Docente a DocenteDto

--- src/main/java/com/schoolSystem/mapper/DocenteToDocenteGetDto.java
+++ src/main/java/com/schoolSystem/mapper/DocenteToDocenteGetDto.java
@@ -1,4 +1,15 @@
 package com.schoolSystem.mapper;
 
-public class DocenteToDocenteGetDto {
+import com.schoolSystem.dto.docenteDto.DocenteGetDto;
+import com.schoolSystem.entities.Docente;
+
+public class DocenteToDocenteGetDto implements IMapper<Docente, DocenteGetDto> {
+
+    @Override
+    public DocenteGetDto map(Docente in) {
+        return new DocenteGetDto(in.getNombre(),
+                in.getApellido(),
+                in.getTelefono(),
+                in.getDireccion());
+    }
 }
