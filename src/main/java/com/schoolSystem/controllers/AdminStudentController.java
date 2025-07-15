afbd90950b5efc44798a71438c2c2f8ce7f95c23
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-15 20:09:41 -0300

    feat: Agregar nuevos endpoints, para actualizar datos de Estudiante y para listar todos los Estudiantes

--- src/main/java/com/schoolSystem/controllers/AdminStudentController.java
+++ src/main/java/com/schoolSystem/controllers/AdminStudentController.java
@@ -1,10 +1,38 @@
 package com.schoolSystem.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.schoolSystem.dto.estudianteDto.EstudianteCreateDto;
+import com.schoolSystem.dto.estudianteDto.EstudianteGetDto;
+import com.schoolSystem.dto.estudianteDto.EstudianteUpdateDto;
+import com.schoolSystem.service.admin.AdminEstudianteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
-@RequestMapping("/admin/student")
+@RequestMapping("/admin")
 public class AdminStudentController {
+    private final AdminEstudianteService estudianteService;
 
+    public AdminStudentController(AdminEstudianteService estudianteService) {
+        this.estudianteService = estudianteService;
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity<String> createStudent(@RequestBody EstudianteCreateDto estudianteCreateDto){
+        estudianteService.createStudent(estudianteCreateDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Estudiante creado con éxito.");
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<Set<EstudianteGetDto>>getAllStudents(){
+        return ResponseEntity.ok(estudianteService.getAllStudents());
+    }
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity<String>updateStudentById(@PathVariable Long id, @RequestBody EstudianteUpdateDto updateDto){
+        estudianteService.updateStudentById(id, updateDto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Estudiante actualizado con éxito");
+    }
 }
