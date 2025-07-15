f8354b1d4a8bdb5a31c17d261ecafc54d3fdf989
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-14 22:27:27 -0300

    feat: Agregar nuevo controlador para Estudiante

--- src/main/java/com/schoolSystem/controllers/AdminStudentController.java
+++ src/main/java/com/schoolSystem/controllers/AdminStudentController.java
@@ -1,4 +1,10 @@
 package com.schoolSystem.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/student")
 public class AdminStudentController {
+
 }
