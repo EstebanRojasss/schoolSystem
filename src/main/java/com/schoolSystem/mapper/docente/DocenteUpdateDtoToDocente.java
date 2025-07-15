a883edcab9267bc51af9c94626732415df1cf39b
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-14 21:49:27 -0300

    refactor: Cambiar/agregar a nuevo paquete especifico para el tipo, además de agregar nuevo parametro de tipo Curso a metodo update

--- src/main/java/com/schoolSystem/mapper/docente/DocenteUpdateDtoToDocente.java
+++ src/main/java/com/schoolSystem/mapper/docente/DocenteUpdateDtoToDocente.java
@@ -1,4 +1,17 @@
 package com.schoolSystem.mapper.docente;
 
-public class DocenteUpdateDtoToDocente {
+import com.schoolSystem.dto.docenteDto.DocenteUpdateDto;
+import com.schoolSystem.entities.Curso;
+import com.schoolSystem.entities.Docente;
+import com.schoolSystem.mapper.mapperInterfaces.IUpdate;
+
+public class DocenteUpdateDtoToDocente implements IUpdate<Docente, DocenteUpdateDto> {
+
+    @Override
+    public void update(Docente docente, DocenteUpdateDto inputDto, Curso curso) {
+        docente.setNombre(inputDto.nombre());
+        docente.setApellido(inputDto.apellido());
+        docente.setNumeroDocumento(inputDto.numeroDocumento());
+        docente.addCurso(curso);
+    }
 }
