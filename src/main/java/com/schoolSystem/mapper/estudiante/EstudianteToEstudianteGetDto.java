84c143e0f5ea25cedd4d22ad7a0d4af008c07417
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-15 19:58:29 -0300

    feat: Agregar nueva clase para mapear entidad Estudiante a EstudianteDto

--- src/main/java/com/schoolSystem/mapper/estudiante/EstudianteToEstudianteGetDto.java
+++ src/main/java/com/schoolSystem/mapper/estudiante/EstudianteToEstudianteGetDto.java
@@ -1,4 +1,28 @@
 package com.schoolSystem.mapper.estudiante;
 
-public class EstudianteToEstudianteGetDto {
+import com.schoolSystem.dto.estudianteDto.EstudianteGetDto;
+import com.schoolSystem.entities.Curso;
+import com.schoolSystem.entities.Estudiante;
+import com.schoolSystem.mapper.mapperInterfaces.IMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class EstudianteToEstudianteGetDto implements IMapper<Estudiante, EstudianteGetDto> {
+
+    @Override
+    public EstudianteGetDto map(Estudiante estudiante) {
+        Set<String> cursos = estudiante.getCursos()
+                .stream()
+                .map(Curso::getNombreCurso)
+                .collect(Collectors.toSet());
+
+        return new EstudianteGetDto(estudiante.getNombre(),
+                estudiante.getApellido(),
+                estudiante.getNumeroDocumento(),
+                estudiante.getTelefono(),
+                cursos);
+    }
 }
