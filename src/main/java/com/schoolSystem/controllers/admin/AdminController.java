f9bac5e0d5aab9010f764d7a7b32a5c5a2c73270
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-07 23:36:11 -0300

    feat: Implementar documentación a los Endpoints de la clase AdminController

--- src/main/java/com/schoolSystem/controllers/admin/AdminController.java
+++ src/main/java/com/schoolSystem/controllers/admin/AdminController.java
@@ -5,6 +5,7 @@ import com.schoolSystem.dto.usuarioDto.UsuarioGetDto;
 import com.schoolSystem.dto.usuarioDto.UsuarioUpdateDto;
 import com.schoolSystem.mapper.usuario.UsuarioToUsuarioDTO;
 import com.schoolSystem.service.admin.AdminUsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +26,14 @@ public class AdminController {
     }
 
     @PostMapping("/user")
+    @Operation(description = "Endpoint encargado de crear nuevos Usuarios")
     public ResponseEntity<Void> createUser(@RequestBody UsuarioCreateDto usuarioCreateDto){
         adminUsuarioService.createUser(usuarioCreateDto);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/user")
+    @Operation(description = "Endpoint encargado de listar todos los usuarios")
     public ResponseEntity<Set<UsuarioGetDto>>getAllUsers(){
         return ResponseEntity.ok(adminUsuarioService.getAllUsers().
                 stream().
@@ -39,18 +42,21 @@ public class AdminController {
     }
 
     @DeleteMapping("/user")
+    @Operation(description = "Endpoint encargado de borrar usuario por email.")
     public ResponseEntity<String> deleteUserByEmail(String email){
         adminUsuarioService.deleteUserByEmail(email);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("user/{id}")
+    @Operation(description = "Endpoint de borrar usuario por Id")
     public ResponseEntity<String> deleteUserById(@PathVariable("id") Long id){
         adminUsuarioService.deleteUserById(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("user/{id}")
+    @Operation(description = "Endpoint encargado de actualizar usuario por Id")
     public ResponseEntity<String> updateUserById(@PathVariable("id") Long id, @RequestBody UsuarioUpdateDto usuarioUpdateDto){
         adminUsuarioService.updateById(id, usuarioUpdateDto);
         return ResponseEntity.ok().build();
