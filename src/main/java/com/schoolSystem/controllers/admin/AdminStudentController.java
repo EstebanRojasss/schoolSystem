2f8cdc3d75cdc863243a969819619331729c17f8
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-07 23:38:36 -0300

    feat: Implementar documentación a los endpoints en la calase AdminStudentController

--- src/main/java/com/schoolSystem/controllers/admin/AdminStudentController.java
+++ src/main/java/com/schoolSystem/controllers/admin/AdminStudentController.java
@@ -4,6 +4,7 @@ import com.schoolSystem.dto.estudianteDto.EstudianteCreateDto;
 import com.schoolSystem.dto.estudianteDto.EstudianteGetDto;
 import com.schoolSystem.dto.estudianteDto.EstudianteUpdateDto;
 import com.schoolSystem.service.admin.AdminEstudianteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,19 +21,25 @@ public class AdminStudentController {
     }
 
     @PostMapping("/student")
+    @Operation(description = "Endpoint encargado de crear nuevo estudiante")
     public ResponseEntity<String> createStudent(@RequestBody EstudianteCreateDto estudianteCreateDto){
         estudianteService.createStudent(estudianteCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Estudiante creado con éxito.");
     }
 
     @GetMapping("/students")
+    @Operation(description = "Endpoint encargado de listar todos los estudiantes")
     public ResponseEntity<Set<EstudianteGetDto>>getAllStudents(){
         return ResponseEntity.ok(estudianteService.getAllStudents());
     }
 
     @PutMapping("/student/{id}")
+    @Operation(description = "Endpoint encargado de actualizar estudiante por Id")
     public ResponseEntity<String>updateStudentById(@PathVariable Long id, @RequestBody EstudianteUpdateDto updateDto){
         estudianteService.updateStudentById(id, updateDto);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Estudiante actualizado con éxito");
     }
+
+
+    
 }
