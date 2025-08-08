9f8a862af679459f905a6db06dacbaffd864bff7
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-08 00:08:27 -0300

    feat: Implementar una descripcion a a la documentacion de los endpoints de la clase controlador AdminTeacherController

--- src/main/java/com/schoolSystem/controllers/admin/AdminTeacherController.java
+++ src/main/java/com/schoolSystem/controllers/admin/AdminTeacherController.java
@@ -4,6 +4,7 @@ import com.schoolSystem.dto.docenteDto.DocenteCreateDto;
 import com.schoolSystem.dto.docenteDto.DocenteGetDto;
 import com.schoolSystem.dto.docenteDto.DocenteUpdateDto;
 import com.schoolSystem.service.admin.AdminDocenteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,22 +22,30 @@ public class AdminTeacherController {
     }
 
     @PostMapping("/teacher")
+    @Operation(summary = "Crea nuevo profesor.",
+    description = "Endpoint encargado de crear nuevo profesor.")
     public ResponseEntity<String>createTeacher(@RequestBody DocenteCreateDto createDto){
         teacherService.createTeacher(createDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
     @PutMapping("teacher/{id}")
-    public ResponseEntity<String>updateTeacher(@RequestBody DocenteUpdateDto updateDto, @PathVariable Long id){
+    @Operation(summary = "Actualiza profesor de acuerdo al Id ingresado",
+    description = "Endpoint encargado de actualizar los datos de un profesor.")
+    public ResponseEntity<String>updateTeacherById(@RequestBody DocenteUpdateDto updateDto, @PathVariable Long id){
         teacherService.updateTeacherById(id, updateDto);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     @GetMapping("teacher/{id}")
+    @Operation(summary = "Obtiene un profesor de acuerdo al Id ingresado",
+    description = "Endpoint encargado de obtener los datos de un profesor.")
     public ResponseEntity<DocenteGetDto>getTeacherById(@PathVariable Long id){
         return ResponseEntity.ok(teacherService.getTeacherById(id));
     }
 
     @GetMapping("/teachers")
+    @Operation(summary = "Lista todos los profesores disponibles.",
+    description = "Endpoint encargado de obtener todos los profesores.")
     public ResponseEntity<Set<DocenteGetDto>>getAlTeachers(){
         return ResponseEntity.ok(teacherService.getAllTeachers());
     }
